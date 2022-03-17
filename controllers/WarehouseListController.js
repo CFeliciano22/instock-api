8cc2ebd35aa9213679549083f7006b800f3cf2d0
Jared Medeiros <jared14jared@gmail.com>
2022-03-17 12:21:11 -0400

    swapped the get one functions between warehouse and invenotry list controllers. fixed the issue of not getting inventory items within the warehouse component. checked by brian cesar jared and felipe

--- controllers/WarehouseListController.js
+++ controllers/WarehouseListController.js
@@ -1,40 +1,39 @@
-const warehouseListModel = require('../models/warehouseListModel.js');
+const warehouseListModel = require("../models/warehouseListModel.js");
+const inventoryListModel = require("../models/inventoryListModel.js");
 
 const getAll = (req, res) => {
-    const warehouseData = warehouseListModel.getAll();
+  const warehouseData = warehouseListModel.getAll();
 
-    const strippedData = warehouseData.map((warehouse) => {
-        
-        return {
-            id: warehouse.id,
-            name: warehouse.name,
-            address: warehouse.address,
-            city: warehouse.city,
-            country: warehouse.category,
-            name: warehouse.contact.name,
-            phone: warehouse.contact.phone,
-            email: warehouse.contact.email
-            
-        }
+  const strippedData = warehouseData.map((warehouse) => {
+    return {
+      id: warehouse.id,
+      name: warehouse.name,
+      address: warehouse.address,
+      city: warehouse.city,
+      country: warehouse.category,
+      contact: warehouse.contact.name,
+      phone: warehouse.contact.phone,
+      email: warehouse.contact.email,
+    };
+  });
 
-    });
-
-    res.json(strippedData);
-}
-
-const getOne = (req, res) => {
-    const { id } = req.params;
-    const foundWarehouse = warehouseModel.getById(id);
-
-    if(!foundWarehouse) {
-        return res.status(404).send('A warehouse does not exist with the id of' + id);
-    }
-
-    res.json(foundWarehouse);
+  res.json(strippedData);
 };
 
-module.exports = 
-{
-getAll,
-getOne
+const getOne = (req, res) => {
+  const { id } = req.params;
+  const foundWarehouse = warehouseListModel.getById(id);
+  const warehouseInventoryList = inventoryListModel
+    .getAll()
+    .filter((item) => item.warehouseID === foundWarehouse.id);
+
+  res.send({
+    warehouse: foundWarehouse,
+    inventoryList: warehouseInventoryList,
+  });
+};
+
+module.exports = {
+  getAll,
+  getOne,
 };
--- controllers/inventoryListController.js
+++ controllers/inventoryListController.js
@@ -1,40 +1,35 @@
-const inventoryListModel = require('../models/inventoryListModel.js');
+const inventoryListModel = require("../models/inventoryListModel.js");
 
 const getAll = (req, res) => {
-    const inventoryData = inventoryListModel.getAll();
+  const inventoryData = inventoryListModel.getAll();
 
-    const strippedData = inventoryData.map((inventory) => {
-        
-        return {
-            id: inventory.id,
-            warehouseID: inventory.warehouseID,
-            warehouseName: inventory.warehouseName,
-            itemName: inventory.itemName,
-            category: inventory.category,
-            status: inventory.status,
-            quantity: inventory.status
-        }
+  const strippedData = inventoryData.map((inventory) => {
+    return {
+      id: inventory.id,
+      warehouseID: inventory.warehouseID,
+      warehouseName: inventory.warehouseName,
+      itemName: inventory.itemName,
+      category: inventory.category,
+      status: inventory.status,
+      quantity: inventory.quantity,
+    };
+  });
 
-    });
-
-    res.json(strippedData);
-}
-
-const getOne = (req, res) => {
-    const { id } = req.params;
-    const foundWarehouse = warehouseModel.getById(id);
-    const inventories = items.filter((item)=> item.warehouseID === req.params.itemid)
-
-    if(!foundWarehouse) {
-        return res.status(404).send('A warehouse does not exist with the id of' + id);
-    }
-
-    res.json(foundWarehouse);
-    res.json(inventories);
+  res.json(strippedData);
 };
 
-module.exports = 
-{
-getAll,
-getOne
+const getOne = (req, res) => {
+  const { id } = req.params;
+  const foundItem = inventoryListModel.getById(id);
+
+  if (!foundItem) {
+    return res.status(404).send("An item does not exist with the id of" + id);
+  }
+
+  res.json(foundItem);
+};
+
+module.exports = {
+  getAll,
+  getOne,
 };
