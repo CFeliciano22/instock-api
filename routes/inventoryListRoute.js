b9cdc7a8db3125c1db4180e94bc355d023263a32
Francisco Deorci <franciscodeorci@Franciscos-MacBook-Pro.local>
2022-03-18 12:36:37 -0400

    added delete inventory item route

--- routes/inventoryListRoute.js
+++ routes/inventoryListRoute.js
@@ -4,5 +4,6 @@ const inventoryListController = require('../controllers/inventoryListController.
 
 router.get('/', inventoryListController.getAll);
 router.get('/:id', inventoryListController.getOne);
+router.get('/:id', inventoryListController.deleteOne);
 
 module.exports = router;
